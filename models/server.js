dc3044e1107ac15da1c8ae4c92d2eac93f111e78
= <pumaronal2016@gmail.com>
2024-01-13 22:19:59 -0400

    Se agrego el Code en la Clase Server en middlewares para trabajar con CORS

--- models/server.js
+++ models/server.js
@@ -8,7 +8,9 @@ const http     = require('http');
 const socketio = require('socket.io');
 
 const path     = require('path');
+const cors     = require('cors');
 const Sockets  = require('./sockets');
+
 
 
 class Server {
@@ -29,6 +31,9 @@ class Server {
     middlewares(){
         //Desplegar el directorio Público
         this.app.use(express.static(path.resolve(__dirname, '../public')))
+
+        //CORS
+        this.app.use(cors());
 
     }
 
